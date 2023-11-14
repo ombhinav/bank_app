00e1600ddc689e053668e946f451cd8fb1e6ca48
ombhinav <abhinavgarg0077@gmail.com>
2023-11-14 13:21:23 +0530

    add billing and cardDeal

--- src/components/CardDeal.js
+++ src/components/CardDeal.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import { card } from '../assets'
+import styles , {layout} from '../style'
+import Button from './Button'
 
 const CardDeal = () => {
   return (
-    <div>
-      
-    </div>
+    <section className= {layout.section}>
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>Find a better card deal <br className='sm:block hidden ' />in few easy steps.</h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Molestias impedit, minus quo necessitatibus dolore, fuga ullam dolorem corrupti dignissimos laborum, facere eum nulla numquam beatae nihil veritatis ipsa quae cupiditate?
+        </p>
+      <Button styles="mt-10" />
+      </div>
+      <div className={layout.sectionImg}>
+        <img src={card} alt = "card" className='w-[100%] h-[100%] '/>
+      </div>
+    </section>
   )
 }
 
