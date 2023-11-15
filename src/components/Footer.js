3ae3e991f41e9f947875ee2bfbb8079e91c074b2
ombhinav <abhinavgarg0077@gmail.com>
2023-11-16 00:33:10 +0530

    initialise build

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
           <p className={`${styles.paragraph} mt-4 max-w-[310px]`}>A new way to make the payments easy, reliable and secure.</p>
         </div>
 
-        <div className="flex-1.5 w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
+        <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10">
           {footerLinks.map((footerLink) => (
             <div key={footerLink.key} className="flex flex-col ss:my-0 my-4 min-w-[150px]" >
               <h4 className='font-poppins font-medium text-[18px] leading-[27px] text-white '>
@@ -30,7 +30,7 @@ const Footer = () => {
         </div>
       </div>
 
-      <div className='w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3f3r45]'>
+      <div className='w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3F3E45]'>
         <p className='font-poppins font-normal text-center text-[18px] leading-[27px] text-white '>
         Copyright Ⓒ 2022 HooBank. All Rights Reserved.
         </p>
@@ -40,7 +40,7 @@ const Footer = () => {
             <img key={social.id}
             src={social.icon}
             alt="social.id"
-            className={`w-[21px] h-[21px] object-contain cursor-pointer ${index != socialMedia.length -1 ? 'mr-6' : 'mr-0'}`} />
+            className={`w-[21px] h-[21px] object-contain cursor-pointer ${index !== socialMedia.length -1 ? 'mr-6' : 'mr-0'}`} />
           ))}
         </div>
       </div>
